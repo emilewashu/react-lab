ffb387631a1995ba9c147a5be46878d1a42683b8
Emile Ntiege <e.a.ntiege@wustl.edu>
2025-11-05 21:00:32 -0600

    Added usestate variable to track the currentmove and added its setter to handleplay and jumpto

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -75,14 +75,18 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setCurrentMove(nextHistory.length - 1);
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
   const moves = history.map((squares, move) => {
     let description;
