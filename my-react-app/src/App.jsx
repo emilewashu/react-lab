f40b0c484e390662ead5a03b38bfe33ad21f6d0e
Emile Ntiege <e.a.ntiege@wustl.edu>
2025-11-05 20:29:33 -0600

    Added game component layout with exp[ort default and updated board to be a normal function

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,7 +6,7 @@ function Square({ value, onSquareClick }) {
     </button>
   );
 }
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -76,4 +76,15 @@ export default function Board() {
   );
 }
 
-// export default Square;
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
